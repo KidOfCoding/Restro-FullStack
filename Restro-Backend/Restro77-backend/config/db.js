a7a03d3385dcb0e40f7a85f1344ec15d3e2c78af
KidOfCoding <myofficial97531@gmail.com>
2025-12-31 02:20:47 +0530

    cors correction

--- Restro-Backend/Restro77-backend/config/db.js
+++ Restro-Backend/Restro77-backend/config/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
+        if (mongoose.connections[0].readyState) {
+            return; // Already connected
+        }
         await mongoose.connect(process.env.MONGO_DBurl);
         console.log("DB Connected");
     } catch (error) {
