58d54183207a2c58137478f1d255b88d0066ca59
KidOfCoding <myofficial97531@gmail.com>
2026-01-11 21:05:49 +0530

    backend issue is solved

--- Restro-Backend/Restro77-backend/server.js
+++ Restro-Backend/Restro77-backend/server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 4000;
 app.use(cors({
     origin: process.env.ALLOWED_ORIGINS
         ? process.env.ALLOWED_ORIGINS.split(',')
-        : ["https://www.restro77.com", "https://admin.restro77.com", "http://localhost:5173", "http://localhost:5174"],
+        : ["https://www.restro77.com", "https://restro77.com", "https://admin.restro77.com", "http://localhost:5173", "http://localhost:5174", "http://localhost:4000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "token", "Origin", "X-Requested-With", "Accept"]
@@ -59,7 +59,7 @@ const io = new Server(server, {
     cors: {
         origin: process.env.ALLOWED_ORIGINS
             ? process.env.ALLOWED_ORIGINS.split(',')
-            : ["https://www.restro77.com", "https://admin.restro77.com", "http://localhost:5173", "http://localhost:5174"],
+            : ["https://www.restro77.com", "https://restro77.com", "https://admin.restro77.com", "http://localhost:5173", "http://localhost:5174", "http://localhost:4000"],
         methods: ["GET", "POST"],
         credentials: true
     }
